c86b150168402dcabc2f25d50e9e5e357e5a2dbf
codemafia <tono1masa@gmail.com>
2021-10-12 17:35:34 +0900

    fix

--- chap17/countdown/public/client.js
+++ chap17/countdown/public/client.js
@@ -4,7 +4,7 @@ function countdown() {
   // h1#countdown要素を取得
   const countdown = document.querySelector("#countdown");
   // 現在のカウントダウンの値を取得
-  let currentCount = parseInt(countdown.textContent);
+  let currentCount = Number(countdown.textContent);
   // countdown.textContentが数値として変換できなかった場合（NaN）
   // または0の場合にはfalsyな値のため、以下のif文で判定する
   if (!currentCount) {
@@ -13,7 +13,7 @@ function countdown() {
   }
   // 1秒ごとのインターバル処理を実行
   const intervalID = setInterval(() => {
-    // カウントダウンの値から1マイナス
+    // カウントダウンの値を1減算
     currentCount--;
     // カウントダウンの値が0のとき
     if (currentCount === 0) {
